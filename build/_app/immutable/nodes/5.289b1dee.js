f50f4b66054bb21002ebb193f777e69b1f13746c
Marcus Wærsted Skogsaas <marcus@skogsaas.no>
2023-09-18 20:07:07 +0200

    v0.10.3

---
+++ build/_app/immutable/nodes/5.289b1dee.js
@@ -0,0 +1,43 @@
+import { s as safe_not_equal, x as component_subscribe, o as onMount } from "../chunks/scheduler.c054974b.js";
+import { S as SvelteComponent, i as init } from "../chunks/index.036fb736.js";
+import { p as page } from "../chunks/stores.a42cabe9.js";
+import { g as goto, A as AthomCloudAPI } from "../chunks/AthomCloudAPI.0ceaa2f3.js";
+import { e as base } from "../chunks/singletons.e19c2d1f.js";
+import { c as clientId, a as clientSecret, b as homey } from "../chunks/homey.65cba409.js";
+function instance($$self, $$props, $$invalidate) {
+  let $page;
+  component_subscribe($$self, page, ($$value) => $$invalidate(0, $page = $$value));
+  let code;
+  onMount(async () => {
+    if (code != null) {
+      const cloudApi = new AthomCloudAPI({ clientId, clientSecret });
+      const loggedIn = await cloudApi.isLoggedIn();
+      if (!loggedIn) {
+        if (cloudApi.hasAuthorizationCode()) {
+          await cloudApi.authenticateWithAuthorizationCode();
+          const user = await cloudApi.getAuthenticatedUser();
+          const firstHomey = await user.getFirstHomey();
+          const homeyApi = await firstHomey.authenticate();
+          homey.set(homeyApi);
+        }
+      }
+    }
+    await goto(base + "/");
+  });
+  $$self.$$.update = () => {
+    if ($$self.$$.dirty & /*$page*/
+    1) {
+      code = $page.url.searchParams.get("code");
+    }
+  };
+  return [$page];
+}
+class Page extends SvelteComponent {
+  constructor(options) {
+    super();
+    init(this, options, instance, null, safe_not_equal, {});
+  }
+}
+export {
+  Page as component
+};

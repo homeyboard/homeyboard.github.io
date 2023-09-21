d8cdd329af5a56eadb001862a9afdc7f7288d08b
Marcus Wærsted Skogsaas <marcus@skogsaas.no>
2023-09-21 19:57:27 +0200

    v0.11.0

---
+++ build/_app/immutable/chunks/navigation.9ae3601a.js
@@ -0,0 +1,5 @@
+import { k as client_method } from "./singletons.16b38e2a.js";
+const goto = /* @__PURE__ */ client_method("goto");
+export {
+  goto as g
+};
---
+++ build/_app/immutable/nodes/5.720251b9.js
@@ -0,0 +1,44 @@
+import { s as safe_not_equal, y as component_subscribe, o as onMount } from "../chunks/scheduler.a4b1dbd5.js";
+import { S as SvelteComponent, i as init } from "../chunks/index.6d45844e.js";
+import { p as page } from "../chunks/stores.7ee4de34.js";
+import { A as AthomCloudAPI } from "../chunks/AthomCloudAPI.108df682.js";
+import { g as goto } from "../chunks/navigation.9ae3601a.js";
+import { e as base } from "../chunks/singletons.16b38e2a.js";
+import { c as clientId, a as clientSecret, b as homey } from "../chunks/homey.e5ef1543.js";
+function instance($$self, $$props, $$invalidate) {
+  let $page;
+  component_subscribe($$self, page, ($$value) => $$invalidate(0, $page = $$value));
+  let code;
+  onMount(async () => {
+    if (code != null) {
+      const cloudApi = new AthomCloudAPI({ clientId, clientSecret });
+      const loggedIn = await cloudApi.isLoggedIn();
+      if (!loggedIn) {
+        if (cloudApi.hasAuthorizationCode()) {
+          await cloudApi.authenticateWithAuthorizationCode();
+          const user = await cloudApi.getAuthenticatedUser();
+          const firstHomey = await user.getFirstHomey();
+          const homeyApi = await firstHomey.authenticate();
+          homey.set(homeyApi);
+        }
+      }
+    }
+    await goto(base + "/");
+  });
+  $$self.$$.update = () => {
+    if ($$self.$$.dirty & /*$page*/
+    1) {
+      code = $page.url.searchParams.get("code");
+    }
+  };
+  return [$page];
+}
+class Page extends SvelteComponent {
+  constructor(options) {
+    super();
+    init(this, options, instance, null, safe_not_equal, {});
+  }
+}
+export {
+  Page as component
+};
